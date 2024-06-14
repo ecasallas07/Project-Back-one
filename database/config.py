f48db948e695a548b361753e974c47a1f2bfbccd
ecasallas07 <ecasallas07@gmail.com>
2024-06-13 20:10:25 -0500

    feat(api): Connection database, creation the first endpoint

--- database/config.py
+++ database/config.py
@@ -5,18 +5,56 @@ class DatabaseGateway:
 
     def db_connect(self):
         db_con = mysql.connector.connect(
-                host = "localhost",
-                user = "root",
-                password  = "4577",
-                database = "blog_api"
+                host = 'localhost',
+                user = 'root',
+                password  = '4577',
+                database = 'blog_api'
                 )
         cursor = db_con.cursor()
         cursor.execute("SELECT DATABASE()")
         data = cursor.fetchone()
-        print("Connection established to: ",data)
+        if data:
+            print("Connection established to: ",data[0])
+        else:
+            print("Connection error")
         return db_con
 
     def db_query(self,query_string):
+        if self.db_connect() is None:
+            raise Exception("No se puede realizar la consulta.")
 
+        my_db= self.db_connect()
+        mycursor= my_db.cursor() 
+        mycursor.execute(query_string)
+
+
+        columnas = mycursor.description
+        data= []
+
+        for row in mycursor.fetchall():
+            row_data = {}
+            for(column_name,column_value) in enumerate(row):
+                row_data[columnas[column_name][0]] = column_value
+
+            data.append(row_data)
+
+        json_object = json.dumps(data) # dumps: convert data to json
+
+        return json_object
+
+
+    def db_execute(self, query_string,data):
         my_db = self.db_connect()
-        mycursor = 
+        mycursor  = my_db.cursor()
+        mycursor.execute(query_string,data)
+        my_db.commit() # save changes in database
+
+        self.lastrowid = str(mycursor.lastrowid) # lastrowid recuperate the id of last row
+        print(f"Elemento guardado con el id: {self.lastrowid}")
+
+# Test connection database
+w1 = DatabaseGateway()
+w1.db_connect()
+
+
+        
--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-
 from typing import Union
 from fastapi import FastAPI
+from database import config
 
-app = FastAPI
+app = FastAPI()
 
-@app.get("/")
+@app.get("/hee")
 def read_root():
-    return{"Hello":"World"}
+    return{"message":"API project one BLOG"}
+
+@app.get("/users")
+def read_users():
+    sql = "SELECT * FROM users"
+    db = config.DatabaseGateway()
+    try:
+        info = db.db_query(sql)
+        return {"user": info}
+    except Exception as e:
+        return {"error":str(e)}
+     
+
+
+#@app.get("/articles"):
+
+
 
 
 
